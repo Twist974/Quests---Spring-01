8edc0a4723fe5dcad8a920bbed3ffe6bcde2a375
gabrielle <gabrielle.kbidi@gmail.com>
2021-06-16 16:23:55 +0200

    DoctorController.java file ok

---
+++ src/main/java/com/wildcodeschool/questSpring01/controller/DoctorController.java
@@ -0,0 +1,29 @@
+package com.wildcodeschool.questSpring01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+	
+	 @GetMapping("/doctor/1")
+	    @ResponseBody
+	    public String giveName1() {
+	        return "William Hartnell";
+	    }
+
+	 @GetMapping("/doctor/10")
+	    @ResponseBody
+	    public String giveName2() {
+	        return "David Tennant";
+	    }
+	 
+	 @GetMapping("/doctor/13")
+	    @ResponseBody
+	    public String giveName3() {
+	        return "Jodie Whittaker";
+	    }
+	 
+	 
+}
